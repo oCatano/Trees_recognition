9c5199d1b55688ca4bdcf7dabb6049b980f13cf0
Bastard12 <rus.akopyan03@mail.ru>
2023-06-08 19:02:17 +0300

    Slight changes

--- parsers.py
+++ parsers.py
@@ -1,29 +1,17 @@
 import shapefile as sh
+import laspy
+from laspy.file import File
+from typing import List
+from tqdm import tqdm
 
 
 def parse_trees_hight(file_path: str) -> list[dict()]:
     '''
+    Функция парсит данные о высотах деревьев
 
     :param: file_path[str] -> Путь к файлу в одного из 5 форматов{.shp; .dbf; .prj; .shx; .cpg} который нужно распарсить.
 
     :return: records[list[dict()]] -> Список из attributes.
-
-    :var: sf[<class 'shapefile.Reader'>] -> прочитанный сырой файл с данными;
-
-          fields[list['str']] -> список из названия полей с sf:
-
-                ['GM_LAYER', 'MAP_NAME', 'NAME', 'LAYER', 'TreeID',
-                'TreeLocati', 'TreeLoca_1', 'TreeHeight', 'CrownDiame',
-                'CrownArea', 'CrownVolum', 'ELEVATION', 'POINT_SYMB', 'FONT_SIZE',
-                'FONT_COLOR', 'FONT_CHARS'];
-
-          shape_record[<class 'shapefile.ShapeRecord'>] -> заключает в себе информацию об одном обьекте из sf  # метод shapeRecords() вернет geometry и attributes для всех обьектов в виде списка объектов ShapeRecord;
-
-          attributes[dict('fields[i]': data)] -> словарь атрибутов обьекта shape_record, содержит ключи:
-                ['TreeID', 'Coord_longitude', 'Coord_latitude', 'TreeHeight', 'CrownDiame',
-                'CrownArea', 'CrownVolum', 'ELEVATION'];
-
-    :add_var: keys[list['str']] -> список из ненужных ключей со списка fields
 
     '''
 
@@ -49,5 +37,4 @@ def parse_trees_hight(file_path: str) -> list[dict()]:
                 records.append(attributes)
 
     return records
-
 
